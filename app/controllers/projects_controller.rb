aee20e9e0b02c47a0db6064f6010854a6aeca056
cnmillar <cnmillar@gmail.com>
2015-07-11 20:47:26 +0000

    add ingredients to project page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,6 +6,8 @@ class ProjectsController < ApplicationController
 
 	def show
 		@project = Project.find(params[:id])
+		@equipment = @project.ingredients.where(ing_type: "equipment")
+		@ingredients = @project.ingredients.where(ing_type: "ingredient")
 	end
 	
 
