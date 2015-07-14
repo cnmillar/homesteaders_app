b5cc2e98b4ae96fb52a7c8d9e863b2ea8b024888
bhammett <beth.hammett@gmail.com>
2015-07-14 02:35:42 +0000

    save user from facebook login

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  
 
 	def index
     @categories = Category.order('name asc').all
--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,6 @@ HeApp::Application.routes.draw do
   get '/login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
-
+  # get '/auth/:provider/callback' => 'users#callback'
+  get '/auth/:provider/callback', to: 'sessions#create'
 end
