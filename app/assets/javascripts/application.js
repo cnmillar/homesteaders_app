d259754f5d4127e7aef2ee8743443d7edefbf4c2
dlgood <dlgood@goodpage.com>
2015-07-16 21:52:16 +0000

    Move YouTube script to app.js along with app.js cleanup"

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,11 +19,6 @@
 //= require new_comment
 //= require_self
 
-if (window.location.hash == '#_=_') {
-    window.location.hash = ''; // for older browsers, leaves a # behind
-    history.pushState('', document.title, window.location.pathname); // nice and clean
-    e.preventDefault(); // no page reload
-}
 
 jQuery(function($){
 
@@ -35,7 +30,6 @@ jQuery(function($){
     speed: 700,
     fade: true,
     cssEase: 'linear'
-
   });
 
 // Video timeline marker placement
@@ -54,127 +48,137 @@ jQuery(function($){
     $('#user-timeline').append(newAvatar);
   });
 
-// Start of hover state for placed markers
+  // Start of hover state for placed markers
   $('.user-avatar').hover(function(){
     clickedAvatar = $(this)
     var avatarFlyout = $('<div />').addClass('user-avatar-flyout').text('This is a comment.');
     clickedAvatar.append(avatarFlyout);
   });
 
+// Comments 
 
-// Facebook login feature
-
-// This is called with the results from from FB.getLoginStatus().
-function statusChangeCallback(response) {
-  console.log('statusChangeCallback');
-  console.log(response);
-  // The response object is returned with a status field that lets the
-  // app know the current login status of the person.
-  // Full docs on the response object can be found in the documentation
-  // for FB.getLoginStatus().
-  if (response.status === 'connected') {
-        // $.getJSON('/auth/facebook/callback', function(json) {
-        //       });
-    'Thanks for logging in, ' + response.name + '!';
-    // Logged into your app and Facebook.
-   FB.api('/me?fields=id,name,email', function(response) {
-      // console.log(JSON.stringify(response));
-     });;
-  } else if (response.status === 'not_authorized') {
-    // The person is logged into Facebook, but not your app.
-    document.getElementById('status').innerHTML = 'Please log ' +
-      'into this app.';
-  } else {
-    // The person is not logged into Facebook, so we're not sure if
-    // they are logged into this app or not.
-    document.getElementById('status').innerHTML = 'Please log ' +
-      'into Facebook.';
-  }
-}
-// This function is called when someone finishes with the Login
-// Button.  See the onlogin handler attached to it in the sample
-// code below.
-function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-}
-window.fbAsyncInit = function() {
-FB.init({
-  appId      : '1643888345868772',
-  cookie     : true,  // enable cookies to allow the server to access 
-                      // the session
-  xfbml      : true,  // parse social plugins on this page
-  version    : 'v2.2' // use version 2.2
-});
-// Now that we've initialized the JavaScript SDK, we call 
-// FB.getLoginStatus().  This function gets the state of the
-// person visiting this page and can return one of three states to
-// the callback you provide.  They can be:
-//
-// 1. Logged into your app ('connected')
-// 2. Logged into Facebook, but not your app ('not_authorized')
-// 3. Not logged into Facebook and can't tell if they are logged into
-//    your app or not.
-//
-// These three cases are handled in the callback function.
-FB.getLoginStatus(function(response) {
-  statusChangeCallback(response);
-});
-};
-
-// Load the SDK asynchronously
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-// Here we run a very simple test of the Graph API after login is
-// successful.  See statusChangeCallback() for when this call is made.
-// function testAPI() {
-//   console.log('Welcome!  Fetching your information.... ');
-//   FB.api('/me', function(response) {
-//     console.log('Successful login for: ' + response.name);
-//     document.getElementById('status').innerHTML =
-//       'Thanks for logging in, ' + response.name + '!';
-//   });
-// }
-
-// FB.logout(function(response) {
-//       // Person is now logged out
-// });
-
-window.fbAsyncInit = function() {
-  FB.init({  
-    appId      : '1643888345868772',
-    xfbml      : true,
-    version    : 'v2.4'
-  });
-};
-
-(function(d, s, id){
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "//connect.facebook.net/en_US/sdk.js";
-   fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-
-});
-
-$(document).ready(function() {
   $(".comments").on("click", ".load-comments .pagination a", function(e)
-  {
-    e.preventDefault();
-    e.stopPropagation();
-    //debugger;
-    $.getScript(this.href, function(data, status, jqxhr) {
-      console.log("DATA", data);
-    });
-    return false;
-  });
+    {
+      e.preventDefault();
+      e.stopPropagation();
+      //debugger;
+      $.getScript(this.href, function(data, status, jqxhr) {
+        console.log("DATA", data);
+      });
+      return false;
+    }
+  );
+
+
+
 });
+
+// Facebook login feature. Extensive comments have been deleted but are available on the Facebook dev site
+
+  // This is called with the results from from FB.getLoginStatus().
+  function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+
+    if (response.status === 'connected') {
+          // $.getJSON('/auth/facebook/callback', function(json) {
+          //       });
+      'Thanks for logging in, ' + response.name + '!';
+      // Logged into your app and Facebook.
+     FB.api('/me?fields=id,name,email', function(response) {
+        // console.log(JSON.stringify(response));
+       });;
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into Facebook.';
+    }
+  }
+  // This function is called when someone finishes with the Login
+  // Button.  See the onlogin handler attached to it in the sample
+  // code below.
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1643888345868772',
+      cookie     : true,  // enable cookies to allow the server to access 
+                          // the session
+      xfbml      : true,  // parse social plugins on this page
+      version    : 'v2.2' // use version 2.2
+    });
+
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+      });
+  };
+
+  // Load the SDK asynchronously
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+    }
+    (document, 'script', 'facebook-jssdk')
+  );
+
+  window.fbAsyncInit = function() {
+    FB.init({  
+      appId      : '1643888345868772',
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
+
+  (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk')
+  );
+
+// Remove garbage from URL after FB login
+  if (window.location.hash == '#_=_') {
+      window.location.hash = ''; // for older browsers, leaves a # behind
+      history.pushState('', document.title, window.location.pathname); // nice and clean
+      e.preventDefault(); // no page reload
+  }
+
+
+
+// YouTube player
+    var tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    var player;
+    function onYouTubeIframeAPIReady() {
+      player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: 'EgdOjyYjjEc',
+        events: {
+
+        }
+      });
+    }
+
+
+
+
 
