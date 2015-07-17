29df27c84f20cbd997eff8b3059565eb483d906c
bhammett <beth.hammett@gmail.com>
2015-07-17 03:08:29 +0000

    email request popup

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook,'1643888345868772','91a1fc735691bdddba3a2fdd27c68ca4',
            :scope => 'email', :display => 'popup'
+
 end
