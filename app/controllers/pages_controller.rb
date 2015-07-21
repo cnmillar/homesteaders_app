d4791220166737e0d041ad9e82bcbd1b164b75fc
dlgood <dlgood@goodpage.com>
2015-07-21 23:44:24 +0000

    Make home categories dynamic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController	
 
 	def home
-		@categories = Category.all.order('name asc')
+		@categories = Category.all.order('name asc')  
+    @popular = Category.find(2,6,8)
   end
 
 	def about
