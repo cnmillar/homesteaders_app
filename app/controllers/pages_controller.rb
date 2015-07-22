bc1d37254e21e3c4ebd4e657d61ec723212c4e39
dlgood <dlgood@goodpage.com>
2015-07-22 00:58:41 +0000

    Attempt at hand-drawn box on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
 	def home
 		@categories = Category.all.order('name asc')  
-    @popular = Category.find(2,6,8)
+    @popular = Category.find(2,8,16)
   end
 
 	def about
