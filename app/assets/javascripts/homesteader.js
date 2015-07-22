308557e1ea3e5a817226b5876aa8fdf065e1cee7
cnmillar <cnmillar@gmail.com>
2015-07-22 20:04:33 +0000

    add hover state on pagination icons

--- app/assets/javascripts/homesteader.js
+++ app/assets/javascripts/homesteader.js
@@ -400,13 +400,13 @@ $(function()
         case 'block': // n and c
             return '<a href="javascript:null;">' + this.value + '</a>';
         case 'next': // >
-            return '        <a href="javascript:null;"><img src="/svg/fast40.svg" width="35px"></a>';
+            return '        <a href="javascript:null;"><img id="next" src="/svg/fast40.svg" width="35px"></a>';
         case 'prev': // <
-            return '    <a href="javascript:null;"><img src="/svg/rewind42.svg" width="35px"></a>';
+            return '    <a href="javascript:null;"><img id="prev" src="/svg/rewind42.svg" width="35px"></a>';
         case 'first': // [
-            return '    <a href="javascript:null;"><img src="/svg/first46.svg" width="40px"></a>';
+            return '    <a href="javascript:null;"><img id="first" src="/svg/first46.svg" width="40px"></a>';
         case 'last': // ]
-            return '    <a href="javascript:null;"><img src="/svg/last17.svg" width="40px"> (' + this.pages + ')</a>';
+            return '    <a href="javascript:null;"><img id="last" src="/svg/last17.svg" width="40px"> (' + this.pages + ')</a>';
         }
       }
     });
